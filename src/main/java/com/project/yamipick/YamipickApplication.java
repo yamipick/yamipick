57998697378f567661a547e0a6c05d4a7273dc16
kijsb1207 <kijsb1207@gmail.com>
2025-11-27 15:00:38 +0900

    프로젝트 생성

---
+++ src/main/java/com/project/yamipick/YamipickApplication.java
@@ -0,0 +1,13 @@
+package com.project.yamipick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamipickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamipickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/yamipick/YamipickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.yamipick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamipickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
