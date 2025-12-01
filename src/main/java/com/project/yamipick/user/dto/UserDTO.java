bcb2e5a710bf5363c8826fba455c9c5c8f46f927
hh-gun <ghdrjs2314@naver.com>
2025-12-01 16:32:19 +0900

    User부분  entity, dto, repository 패키지 및 파일 생성 완

---
+++ src/main/java/com/project/yamipick/user/dto/UserDTO.java
@@ -0,0 +1,29 @@
+package com.project.yamipick.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+	
+	private Long seqUser;
+	private String name;
+	private String id;
+	private String password;
+	private String email;
+	private String phone;
+	private String role;
+	private Integer penaltyScore;
+	private String nickname;
+	private String statusUser;
+
+}
