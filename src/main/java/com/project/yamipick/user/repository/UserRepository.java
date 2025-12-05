4bb4b583eda0e0154405719ae6792306e6937480
hh-gun <ghdrjs2314@naver.com>
2025-12-05 17:44:52 +0900

    gpt 참고

--- src/main/java/com/project/yamipick/user/repository/UserRepository.java
+++ src/main/java/com/project/yamipick/user/repository/UserRepository.java
@@ -8,5 +8,6 @@ import com.project.yamipick.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     
-    Optional<User> findByUserId(String userId); 
+    Optional<User> findByUserId(String userId);
+    
 }
